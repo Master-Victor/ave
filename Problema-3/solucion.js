159580e72c1b731afe98bb858700474170326018
Victor Villafañe <vvillafane@frba.utn.edu.ar>
2023-07-31 00:07:56 -0300

    Solucion 3 - Se agrego una mejor manera de tomar los errores

--- Problema-3/solucion.js
+++ Problema-3/solucion.js
@@ -7,22 +7,23 @@
 function validator(password) {
     let number = 0;
     let caracterEspecial = 0;
+    const errors = [];
     const caracteresEspeciales = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '+', '=', '?'];
-    if (password.length <= 16) return "Menos de 16 caracteres";                        //Pass con menos de 16 caracteres
-    if (password === password.toUpperCase()) return "Sin minusculas";          //Pass con mayusculas
-    if (password === password.toLowerCase()) return "Sin Mayusculas";          //Pass con minusculas
-    if ( password.includes(" ") ) return "Con espacios en blanco";                     //Pass con espacios en blanco
-    if ( password.includes("0") ) return "Con 0";                     //Pass con 0
+    if (password.length <= 16) errors.push("Menos de 16 caracteres");                        //Pass con menos de 16 caracteres
+    if (password === password.toUpperCase()) errors.push("Sin minusculas");          //Pass con mayusculas
+    if (password === password.toLowerCase())  errors.push("Sin Mayusculas");          //Pass con minusculas
+    if ( password.includes(" ") ) errors.push("Con espacios en blanco");                     //Pass con espacios en blanco
+    if ( password.includes("0") ) errors.push("Con 0");                     //Pass con 0
 
     for (let i = 0; i < password.length; i++) {
         if ( caracteresEspeciales.includes(password[i]) ) caracterEspecial+=1;     //Pass con caracteres especiales
         if ( isNaN(password[i]) ) number += 1;                                     //Pass con numeros
-        if ( number < 4 && (password.length - 1) === i ) return "No tiene 4 numeos";             //Pass con mas de 4 numeros  
-        if ( password[i] === password[i - 1] ) return "Caracteres seguidos";                       //Contiene dos letras/numeros iguales consecutivas
-        if ( caracterEspecial < 2 && (password.length - 1) === i ) return "Sin dos caracteres especiales !@#$%ˆ&*-_+=?";   // Pass con mas de 2 caracteres especiales
+        if ( number < 4 && (password.length - 1) === i ) errors.push("No tiene 4 numeros");             //Pass con mas de 4 numeros  
+        if ( password[i] === password[i - 1] ) errors.push("Caracteres seguidos");                       //Contiene dos letras/numeros iguales consecutivas
+        if ( caracterEspecial < 2 && (password.length - 1) === i ) errors.push("Sin dos caracteres especiales !@#$%ˆ&*-_+=?");   // Pass con mas de 2 caracteres especiales
     }
-    return true;
+    return errors;
 }
 
 console.log(validator('Abcdefghijklmnñopqrstuvwxyz123456789!@#$%^&*-_+=?')); // true
-console.log(validator('aBcdefghijklmnñopqrstuvwxyz12345678*9')); // false
+console.log(validator('aBcdefghijklmnñopqrstuvwxyz12345678*90')); // false
